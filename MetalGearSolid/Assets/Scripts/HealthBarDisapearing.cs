c23be83adf6047522fcc5129ce077ab1eb025c03
neocccro <cavandiepen@gmail.com>
2018-02-02 23:53:13 +0100

    everything

---
+++ MetalGearSolid/Assets/Scripts/HealthBarDisapearing.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarDisapearing : MonoBehaviour {
+
+    [SerializeField]
+    private Image healthbar;
+    public bool apear;
+    public bool disapear;
+    public bool pause;
+    private float speed = 2f;
+
+	// Update is called once per frame
+	void Update () {
+        if (apear)
+        {
+            StopAllCoroutines();
+            StartCoroutine(Apear(speed));
+            apear = !apear;
+        }
+        if (disapear)
+        {
+            StopAllCoroutines();
+            StartCoroutine(Disapear(speed));
+            disapear = !disapear;
+        }
+        if (pause)
+        {
+            StopAllCoroutines();
+            pause = !pause;
+        }
+    }
+
+    private IEnumerator Disapear(float acceleration)
+    {
+        while (healthbar.transform.position.y - healthbar.canvas.pixelRect.height < healthbar.rectTransform.rect.height)
+        {
+            healthbar.transform.position = new Vector3(healthbar.transform.position.x, healthbar.transform.position.y + acceleration, healthbar.transform.position.z);
+            yield return 0;
+        }
+    }
+
+    private IEnumerator Apear(float acceleration)
+    {
+        while (healthbar.transform.position.y - healthbar.canvas.pixelRect.height > -healthbar.rectTransform.rect.height)
+        {
+            healthbar.transform.position = new Vector3(healthbar.transform.position.x, healthbar.transform.position.y - acceleration, healthbar.transform.position.z);
+            yield return 0;
+        }
+    }
+}
---
+++ MetalGearSolid/Library/Collab/Original/Assets/Scripts/HealthBarDisapearing.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarDisapearing : MonoBehaviour {
+
+    [SerializeField]
+    private Image healthbar;
+    public bool apear;
+    public bool disapear;
+    public bool pause;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (apear)
+        {
+            StopAllCoroutines();
+            StartCoroutine(Apear());
+            apear = !apear;
+        }
+        if (disapear)
+        {
+            StopAllCoroutines();
+            StartCoroutine(Disapear());
+            disapear = !disapear;
+        }
+        if (pause)
+        {
+            StopAllCoroutines();
+            pause = !pause;
+        }
+        Debug.Log(healthbar.transform.position.y - healthbar.canvas.pixelRect.height);
+    }
+
+    private IEnumerator Disapear()
+    {
+        while (healthbar.transform.position.y - healthbar.canvas.pixelRect.height < healthbar.rectTransform.rect.height)
+        {
+            healthbar.transform.position = new Vector3(healthbar.transform.position.x, healthbar.transform.position.y + 1f, healthbar.transform.position.z);
+            yield return 0;
+        }
+    }
+
+    private IEnumerator Apear()
+    {
+        while (healthbar.transform.position.y - healthbar.canvas.pixelRect.height > -healthbar.rectTransform.rect.height)
+        {
+            healthbar.transform.position = new Vector3(healthbar.transform.position.x, healthbar.transform.position.y - 1f, healthbar.transform.position.z);
+            yield return 0;
+        }
+    }
+}
